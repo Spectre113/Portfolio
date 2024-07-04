aa5dd8b62294e6f44bed75d3b7aa005f8d952bbe
Владимир Топорков <VTvolody626@mail.ru>
2024-07-04 15:38:52 +0700

    about section updated

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,18 +1,51 @@
 import React, { useEffect } from 'react';
-import { useSwiper } from '../hooks/useSwiper';
+import Swiper from 'swiper';
 
 const About: React.FC = () => {
-    useSwiper();
-  
-    useEffect(() => {
-      const swiperClickElements = document.querySelectorAll('.about__swiper-click');
-      swiperClickElements.forEach(swiperClickElement => {
-        const aboutContent = swiperClickElement.closest('.about__swiper-content');
-        aboutContent!.addEventListener('click', () => {
-          swiperClickElement.classList.toggle('about__swiper-click--active');
-        });
+  useEffect(() => {
+    const updateSwiperText = (swiperInstance: any) => {
+      const currentIndex = Math.floor(swiperInstance.realIndex / swiperInstance.params.slidesPerGroup) + 1;
+      const totalSlides = swiperInstance.slides.length - swiperInstance.loopedSlides - 1;
+      document.querySelector('.swiper-text')!.textContent = `${currentIndex} / ${totalSlides}`;
+    };
+
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 2,
+      centeredSlides: false,
+      slidesPerGroup: 2,
+      grabCursor: true,
+      keyboard: {
+        enabled: true,
+      },
+      loop: true,
+      spaceBetween: 50,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+      },
+      on: {
+        init(this: any) {
+          updateSwiperText(this);
+        },
+        slideChange(this: any) {
+          updateSwiperText(this);
+        },
+      },
+    });
+
+    const swiperClickElements = document.querySelectorAll('.about__swiper-click');
+    swiperClickElements.forEach(swiperClickElement => {
+      const aboutContent = swiperClickElement.closest('.about__swiper-content');
+      aboutContent!.addEventListener('click', () => {
+        swiperClickElement.classList.toggle('about__swiper-click--active');
       });
-    }, []);
+    });
+
+  }, []);
 
   return (
     <section id="about" className="about">
