1e11e3c7d83d15d6ec8f3c4c911deb1d2c1dc51b
Владимир Топорков <VTvolody626@mail.ru>
2024-07-06 22:54:52 +0700

    fixed for gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: process.env.NODE_ENV === 'production' ? '/username.github.io' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/username.github.io/' : '',
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
